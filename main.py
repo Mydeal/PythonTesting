544d551188661ad6d95b9fc1ee0a852da6aa3a7b
JohnRuthh <kamilbartosiak11@gmail.com>
2019-02-13 17:03:07 +0100

    Taki se commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome()
+driver.minimize_window()
+driver.get("https://news.google.com")
+#time.sleep(20)
+driver.get_screenshot_as_file("Andrzej.jpg")
+buisnes = driver.find_element_by_class("ICsaqd").click()
+driver.quit()
+
