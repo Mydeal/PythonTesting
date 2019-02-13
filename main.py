f5fa65bb274b568bf8973584dfa64315e71cb21b
JohnRuthh <kamilbartosiak11@gmail.com>
2019-02-13 17:15:29 +0100

    screenshot extension fix

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ driver = webdriver.Chrome()
 driver.minimize_window()
 driver.get("https://news.google.com")
 #time.sleep(20)
-driver.get_screenshot_as_file("Andrzej.jpg")
+driver.get_screenshot_as_file("Andrzej.png")
 buisnes = driver.find_element_by_class("ICsaqd").click()
 driver.quit()
 
