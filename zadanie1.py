472c3433e7364f86a97e03c7fa34c005229ab250
unknown <mariusz.kuchalski@wp.pl>
2019-02-13 18:44:52 +0100

    working program

--- zadanie1.py
+++ zadanie1.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
-
+import time
 a = webdriver.Chrome()
 a.get("https://news.google.com")
-button = a.find_element_by_xpath("//span[@class='ICsaqd']")
+a.maximize_window() 
+button = a.find_element_by_xpath("//div[@aria-label = 'Nauka i technika'] //a[@class = 'SFllF'] [@tabindex = '0']")
 button.click()
+time.sleep(5)
 a.get_screenshot_as_file("photo.png")
 
 a.quit()
